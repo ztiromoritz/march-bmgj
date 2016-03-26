fbc96fa7678f1d177c507ec3509414810f54ee9a
Adam <adam@adamslair.net>
2016-03-26 19:51:44 +0100

    no message

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BerlinJam
+{
+	public class MusicManager : MonoBehaviour
+	{
+		[SerializeField] private AudioClip[] musicLayers = null;
+		[SerializeField] private AudioSource[] audioSources = null;
+
+		private void Start()
+		{
+			for (int i = 0; i < musicLayers.Length; i++)
+			{
+				audioSources[i].clip = musicLayers[i];
+				audioSources[i].Play();
+			}
+		}
+	}
+}
