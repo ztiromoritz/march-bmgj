54360dbff092cbaa3de14013d5e0fcf6f807d4d2
Adam <adam@adamslair.net>
2016-03-26 19:22:00 +0100

    MusicBlock WiP

---
+++ Assets/Scripts/MusicBlock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace BerlinJam
+{
+	public class MusicBlock : MonoBehaviour
+	{
+		[SerializeField] private Renderer fillRenderer = null;
+		[Range(0, 1)]
+		[SerializeField] private float fillState = 1.0f;
+		[SerializeField] private Material blockMaterial = null;
+
+		private Material material;
+
+		private void Awake()
+		{
+			this.material = new Material(this.blockMaterial);
+			this.fillRenderer.material = this.material;
+		}
+		private void Update()
+		{
+			this.material.SetFloat("_FillState", this.fillState);
+		}
+	}
+}
